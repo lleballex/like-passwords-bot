b75ce703949b893dbd22e7ed7924e7a5f179e86b
lleballex <lleballex@yandex.ru>
2022-08-04 11:32:49 +0300

    Added migrate command

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 from aiogram import executor
 
+import sys
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -7,7 +8,15 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
     load_dotenv(Path(__file__).resolve().parent.parent / '.env')
 
-    import handlers
-    from misc import dp
+    if len(sys.argv) == 1:
+        import handlers
+        from misc import dp
 
-    executor.start_polling(dp, skip_updates=True)
+        executor.start_polling(dp, skip_updates=True)
+    elif sys.argv[1] == 'migrate':
+        from misc import db
+        from models import User, Password
+
+        db.create_tables([User, Password])
+
+        print('Successfully migrated')
